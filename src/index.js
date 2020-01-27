731f3f115a7c403aaf4f9d1e6fb1292de44cac0f
Alexander <sashok935@gmail.com>
2020-01-27 22:52:34 +0300

    Add hot browser reload

--- src/index.js
+++ src/index.js
@@ -1,22 +1,29 @@
 const path = require('path')
+const http = require('http')
+const reload = require('reload')
 const express = require('express')
 const nunjucks = require('nunjucks')
 
 const mongoose = require('./config/database')
-
-const pagesRoutes = require('./routes/pages')
+const userRoutes = require('./routes/users')
 
 const app = express()
 const port = process.env.PORT || 3001
 
+mongoose.connection.on('error', (err) => console.error('MongoDB connection error', err))
 nunjucks.configure(path.join(__dirname, 'views'), {
   autoescape: true,
   express: app
 })
-mongoose.connection.on('error', (err) => console.error('MongoDB connection error', err))
 
-app.use('/', pagesRoutes)
+app.use('/users', userRoutes)
 
-app.listen(port, () => {
-  console.log(`Listening on port ${port}!`)
+const server = http.createServer(app)
+
+reload(app).then(() => {
+  server.listen(port, () => {
+    console.log(`Listening on port ${port}!`)
+  })
+}).catch((err) => {
+  console.error('Reload could not start, could not start server app', err)
 })
