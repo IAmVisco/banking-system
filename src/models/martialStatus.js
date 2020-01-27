b020791282253f8eca3be7c409dbcbb92b207e2e
Alexander <sashok935@gmail.com>
2020-01-27 23:48:24 +0300

    Add template data fetching and rendering

--- src/models/martialStatus.js
+++ src/models/martialStatus.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
 const MartialStatusSchema = new Schema({
-  status: { type: String, required: true, trim: true }
+  name: { type: String, required: true, trim: true }
 })
 
-module.exports = mongoose.model('City', MartialStatusSchema)
+module.exports = mongoose.model('MartialStatus', MartialStatusSchema)
--- src/routes/users.js
+++ src/routes/users.js
@@ -2,12 +2,26 @@ const express = require('express')
 const { asyncHandler } = require('../utils')
 const usersService = require('../services/UsersService')
 
+// const City = require('../models/city')
+// const Citizenship = require('../models/citizenship')
+// const Disability = require('../models/disability')
+// const MartialStatus = require('../models/martialStatus')
+// const User = require('../models/user')
+
 const router = express.Router()
 
 router.get('/', asyncHandler(async (req, res) => {
   const users = await usersService.getAllUsers()
 
-  res.render('content.njk', { users })
+  res.render('users.njk', { users })
+}))
+
+router.post('/', asyncHandler(async (req, res) => {
+  await usersService.createUser(req)
+
+  const users = await usersService.getAllUsers()
+
+  res.render('users.njk', { users })
 }))
 
 module.exports = router
